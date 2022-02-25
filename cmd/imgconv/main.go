4ef9c9e46022f08b43b5f0e095b47301ec929162
Takuya Ueda <uedatakuya@gmail.com>
2022-02-25 14:04:41 +0900

    Add -v option

--- cmd/imgconv/main.go
+++ cmd/imgconv/main.go
@@ -9,16 +9,25 @@ import (
 )
 
 var (
-	flagTo   = imgconv.PNG
-	flagFrom = imgconv.TIFF
+	flagTo      = imgconv.PNG
+	flagFrom    = imgconv.TIFF
+	flagVersion bool
 )
 
 func init() {
 	flag.Var(&flagTo, "to", "after format")
 	flag.Var(&flagFrom, "from", "before format")
+	flag.BoolVar(&flagVersion, "v", false, "print version")
 }
 
 func main() {
+	flag.Parse()
+
+	if flagVersion {
+		fmt.Println("v0.2.0")
+		os.Exit(0)
+	}
+
 	if err := imgconv.ConvertAll(os.Args[1], flagFrom, flagTo); err != nil {
 		fmt.Fprintln(os.Stderr, "エラー:", err)
 		os.Exit(1)
